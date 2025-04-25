a8d366d84512970aad5c34ef7cf037e381ca3068
youhei-takahashi <y.takahashi@morijyobi.ac.jp>
2025-04-25 14:23:04 +0900

    init

---
+++ src/test/java/com/example/musicmanagement/MusicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
